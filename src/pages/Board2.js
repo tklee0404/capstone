d26ef38b133b7fa69d1824e95bec5a1626a96251
tklee0404 <taklee040463@gmail.com>
2024-11-26 20:11:02 +0900

    board2.js added

---
+++ src/pages/Board2.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  useNavigate,
+  BrowserRouter,
+} from "react-router-dom";
+import { useLocation } from "react-router-dom";
+
+import Headerbar from "../UI/header.js";
+
+function Board2 () {
+
+
+    return (
+        <div>
+            <Headerbar></Headerbar>
+            <div></div>
+        </div>
+    );
+}
+
+export default Board2
